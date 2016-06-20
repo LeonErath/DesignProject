b408d925f0927a3ed865982026a2ce2e6ef2342c
LeonErath <leon-erath@hotmail.de>
2016-06-20 02:16:24 +0200

    Initial commit

---
+++ app/src/main/java/leon/designproject/Resize.java
@@ -0,0 +1,43 @@
+package leon.designproject;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+import com.victor.loading.rotate.RotateLoading;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Leon E on 20.06.2016.
+ */
+public class Resize extends Animation{
+
+    final int startWidth;
+    final int targetWidth;
+    View view;
+
+    public Resize(View view, int targetWidth) {
+        this.view = view;
+        this.targetWidth = targetWidth;
+        startWidth = view.getWidth();
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        int newWidth = (int) (startWidth + (targetWidth - startWidth) * interpolatedTime);
+        view.getLayoutParams().width = newWidth;
+        view.requestLayout();
+    }
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+        super.initialize(width, height, parentWidth, parentHeight);
+    }
+
+    @Override
+    public boolean willChangeBounds() {
+        return true;
+    }
+}
